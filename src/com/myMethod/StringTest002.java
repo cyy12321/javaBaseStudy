21a9bfdb9c6045e0f88bd25c8f38aa1ca01842cc
cyy12321 <2363750874@qq.com>
2022-03-02 21:10:32 +0800

    111

--- src/com/myMethod/StringTest002.java
+++ src/com/myMethod/StringTest002.java
@@ -2,7 +2,7 @@ package com.myMethod;
 
 import java.util.Scanner;
 
-public class StrringTest002 {
+public class StringTest002 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入字符串：");
